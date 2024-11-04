4eb6949a06a96f6f983efcdf3bc60b79b575aacc
Gautam Lodwal <115150191+GautamL17@users.noreply.github.com>
2024-11-05 00:30:19 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,25 +7,32 @@ export default {
   ],
   theme: {
     extend: {
-      animation: {
-        'loop-scroll': 'loop-scroll 10s linear infinite',
-        rotate: 'rotate 10s linear infinite',
-      },
       keyframes: {
-        'loop-scroll': {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-        rotate: {
-          '0%': { transform: 'rotate(0deg) scale(1)' },
-          '25%': { transform: 'rotate(90deg) scale(1, 1.5)' },
-          '50%': { transform: 'rotate(180deg) scale(1.5, 1)' },
-          '75%': { transform: 'rotate(270deg) scale(1, 1.5)' },
-          '100%': { transform: 'rotate(360deg) scale(1)' },
-        },
+        scaleUp:{
+          '0%':{
+            opacity:'0.2',
+            transform: 'scale(1)'
+          },
+          '25%':{
+            opacity:'1',
+            transform: 'scale(1)'
+          },
+          '50%':{
+            opacity:'1',
+            transform:'scale(1)'
+          },
+          '75%':{
+            opacity:'1',
+            transform:'scale(1)'
+          },
+          '100%':{
+            opacity:'0.2',
+            transform:'scale(1)'
+          },
+        }
       },
-      boxShadow: {
-        '3xl': '10px 35px 60px -15px rgba(127, 17, 224, 1)',
+      animation: {
+        scaleUp:'scaleUp 2s ease-in-out infinite',
       },
     },
   },
